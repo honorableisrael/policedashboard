8a7fdf9057b7baf76b0c65bfc49c7796756dd8af
Hilary <israel.hilary7@yahoo.com>
2021-02-26 15:47:42 +0100

    completed side nav for mobile view

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
 function myShowHideFunction() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-     return x.type = "text";
-    } else {
-      x.type = "password";
-    }
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    return (x.type = "text");
+  } else {
+    x.type = "password";
   }
+}
+
+function showhide() {
+  console.log("showing");
+  let element = document.getElementById("mobilesideclass");
+  element.classList.toggle("firstshow");
+  // element.className.splice(1,0)
+}
+function hidesidenav() {
+  let element = document.getElementById("mobilesideclass");
+  console.log(element.className.split(""))
+  if(!element.className.includes("secondhide")){
+    // element.className= [""]
+    console.log("hidinghere")
+    return
+  };
+  console.log("oops");
+  element.classList.toggle("secondhide");
+}
